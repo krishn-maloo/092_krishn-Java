034d8571ee08507cc690ee061b54c77a3f072c2a
krishn-maloo <98144812+krishn-maloo@users.noreply.github.com>
2023-02-02 11:43:42 +0530

    Create multithreading.java

---
+++ multithreading.java
@@ -0,0 +1,37 @@
+class Call implements Runnable
+{
+	String a;
+	int x,time;
+	Thread t;
+	Call(String tn,int ti,int ex)
+	{
+		a=tn;
+		x=ex;
+		time=ti;
+		t=new Thread(this,a);
+		t.start();
+	}
+	public void run()
+	{
+		try{
+			for(int i=0;i<x	;i++)
+			{
+				System.out.println(a);
+				Thread.sleep(time);
+			}
+		    }
+		catch(InterruptedException ie)
+		{
+			System.out.println("Inturrupted ");
+		}
+	}
+}
+
+class Lab8
+{
+public static void main(String xx[])
+{
+	new Call("BMS College of Enginnering",10000,2);
+	new Call("CSE",2000,10);
+}
+}
